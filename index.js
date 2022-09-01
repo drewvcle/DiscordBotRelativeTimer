953346e7aa36a4202bb05b2cb7d960f203480f4b
drewvcle <88847563+drewvcle@users.noreply.github.com>
2022-09-01 18:57:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,26 +2,32 @@ const { Client, GatewayIntentBits, Partials, Collection } = require("discord.js"
 const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
 const { User, Message, GuildMember, ThreadMember } = Partials;
 const { userMention, memberNicknameMention, channelMention, roleMention } = require('discord.js');
-// const id = '123456789012345678';
-// const user = userMention(id);
-// const nickname = memberNicknameMention(id);
-// const channel = channelMention(id);
-// const role = roleMention(id);
-
 const { time } = require('discord.js');
-const date1 = new Date();
-const date2 = new Date();
-const date3 = new Date();
-var relative1 = time(date1, 'R');
-var relative2 = time(date2, 'R');
-var relative3 = time(date3, 'R');
-//const relative1 = time(date, 'R');
-// const timeString = time(date);
 
+// Get the difference between two relative times
+function compareTime (s1, s2){
+  var diff;
+  s1 = parseInt(s1.substring(3,13));
+  s2 = parseInt(s2.substring(3,13));
+  return (s2 - s1);
+}
+
+// Constants for auction channels
+var numAucs = 8;
+const channelIDs = ['1013859742210342982', '1013859762284265513', '1013859785365524552', '1015016976978018325', '1015016994552164432', '1015017007390933113', '1015017021592834139', '1015017038852390952'];
+const dates = [];
+var relatives = [];
+for (let i = 0; i < numAucs; i++){
+  dates[i] = new Date();
+  relatives[i] = time(dates[i], 'R');
+}
+
+// Discord stuff
 const client = new Client({
   intents: [Guilds, GuildMembers, GuildMessages],
   partials: [User, Message, GuildMember, ThreadMember],
 });
+
 
 
 client.config = require("./config.json");
@@ -32,38 +38,40 @@ client.on('interactionCreate', async interaction => {
 
 	const { commandName } = interaction;
 
-if (commandName === 'server') {
+  if (commandName === 'server') {
 		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
 	} else if (commandName === 'user') {
 		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: <@${interaction.user.id}>`);
-	} else if (commandName === 'test1') {
-    await interaction.reply(`Channel 1: <#${interaction.channel.id}>`);
-  } else if (commandName === 'timetest') {
-    await interaction.reply(timeString);
-  } else if (commandName === 'auc1') {
-    const date1 = new Date();
-    relative1 = time(date1, 'R');
-    await interaction.reply(relative1);
-  } else if (commandName === 'auc2') {
-    const date2 = new Date();
-    relative2 = time(date2, 'R');
-    await interaction.reply(relative2);
-  } else if (commandName === 'auc3') {
-    const date3 = new Date();
-    relative3 = time(date3, 'R');
-    await interaction.reply(relative3);
+	// } else if (commandName === 'test1') {
+  //   await interaction.reply(`Channel 1: <#${interaction.channel.id}>`);
+  // } else if (commandName === 'timetest') {
+  //   await interaction.reply(timeString);
+
+  // Resets the auction cooldown. Ex. /auc1 would reset the cooldown for Auction #1
+  } else if (commandName.substring(0,3) === 'auc') {
+      let aucnum = parseInt(commandName.substring(3,4));
+      dates[aucnum] = new Date();
+      relatives[aucnum] = time(dates[aucnum], 'R');
+      await interaction.reply(relatives[aucnum]);
+
+  // Previews all auction timers. Ex. /timers
   } else if (commandName === 'timers'){
-    await interaction.reply(`${channelMention('1013859742210342982')}: started ` + relative1 + `.
-${channelMention('1013859762284265513')}: started` + relative2 + `.
-${channelMention('1013859785365524552')}: started ` + relative3 + `.`);
+    const datenow = new Date();
+    var dn = time(datenow);
+
+    for (let i = 0; i < numAucs; i++){
+      if(compareTime(relatives[i], dn) > 86400)
+        relatives[i] = "DONE";
+    }
+    let messageTimes = "";
+    for (let i = 0; i < numAucs; i++)
+      messageTimes += `${channelMention(channelIDs[i])}: ` + relatives[i] + '.\n';
+
+    await interaction.reply(messageTimes);
   }
 });
 
-
-
 client.login(client.config.token);
-
-
 
 
 
